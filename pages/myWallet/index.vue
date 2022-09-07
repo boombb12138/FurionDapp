7dbb56a003edc3b2e64f1e03eda897ccd2662823
LidoKing <55234791+LidoKing@users.noreply.github.com>
2022-09-07 17:38:20 +0800

    feat: loader component

--- pages/myWallet/index.vue
+++ pages/myWallet/index.vue
@@ -54,118 +54,6 @@
 .lockBorder {
   border: 2px solid rgba(255, 255, 255, 0.6) !important;
 }
-
-.sk-folding-cube {
-  margin: 20px auto;
-  width: 50px;
-  height: 50px;
-  position: relative;
-  -webkit-transform: rotateZ(45deg);
-          transform: rotateZ(45deg);
-}
-.sk-folding-cube .sk-cube {
-  float: left;
-  width: 50%;
-  height: 50%;
-  position: relative;
-  -webkit-transform: scale(1.1);
-      -ms-transform: scale(1.1);
-          transform: scale(1.1); 
-}
-.sk-folding-cube .sk-cube:before {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: white;
-  -webkit-animation: sk-foldCubeAngle 2.4s infinite linear both;
-          animation: sk-foldCubeAngle 2.4s infinite linear both;
-  -webkit-transform-origin: 100% 100%;
-      -ms-transform-origin: 100% 100%;
-          transform-origin: 100% 100%;
-}
-.sk-folding-cube .sk-cube2 {
-  -webkit-transform: scale(1.1) rotateZ(90deg);
-          transform: scale(1.1) rotateZ(90deg);
-}
-.sk-folding-cube .sk-cube3 {
-  -webkit-transform: scale(1.1) rotateZ(180deg);
-          transform: scale(1.1) rotateZ(180deg);
-}
-.sk-folding-cube .sk-cube4 {
-  -webkit-transform: scale(1.1) rotateZ(270deg);
-          transform: scale(1.1) rotateZ(270deg);
-}
-.sk-folding-cube .sk-cube2:before {
-  -webkit-animation-delay: 0.3s;
-          animation-delay: 0.3s;
-}
-.sk-folding-cube .sk-cube3:before {
-  -webkit-animation-delay: 0.6s;
-          animation-delay: 0.6s; 
-}
-.sk-folding-cube .sk-cube4:before {
-  -webkit-animation-delay: 0.9s;
-          animation-delay: 0.9s;
-}
-@-webkit-keyframes sk-foldCubeAngle {
-  0%, 10% {
-    -webkit-transform: perspective(140px) rotateX(-180deg);
-            transform: perspective(140px) rotateX(-180deg);
-    opacity: 0; 
-  } 25%, 75% {
-    -webkit-transform: perspective(140px) rotateX(0deg);
-            transform: perspective(140px) rotateX(0deg);
-    opacity: 1; 
-  } 90%, 100% {
-    -webkit-transform: perspective(140px) rotateY(180deg);
-            transform: perspective(140px) rotateY(180deg);
-    opacity: 0; 
-  } 
-}
-
-@keyframes sk-foldCubeAngle {
-  0%, 10% {
-    -webkit-transform: perspective(140px) rotateX(-180deg);
-            transform: perspective(140px) rotateX(-180deg);
-    opacity: 0; 
-  } 25%, 75% {
-    -webkit-transform: perspective(140px) rotateX(0deg);
-            transform: perspective(140px) rotateX(0deg);
-    opacity: 1; 
-  } 90%, 100% {
-    -webkit-transform: perspective(140px) rotateY(180deg);
-            transform: perspective(140px) rotateY(180deg);
-    opacity: 0; 
-  }
-}
-
-.bouncing {
-  position: relative;
-  -webkit-box-reflect: below 15px linear-gradient(transparent, rgba(0,0,0,.3));
-  font-size: 25px;
-  font-weight: 500;
-  margin-top: 50px;
-}
-.bouncing span {
-  position: relative;
-  display: inline-block;
-  color: rgba(255, 255, 255, 0.8);
-  text-transform: uppercase;
-  animation: bounce 2s infinite;
-  animation-delay: calc(.1s * var(--i));
-  
-}
-@keyframes bounce {
-  0%,40%,100% {
-    transform: translateY(0)
-  }
-  20% {
-    transform: translateY(-20px)
-  }
-}
 </style>
 
 <template>
@@ -211,30 +99,7 @@
         </div>
       </div>
     </div>
-    <div v-if="ready === false" class="flex justify-center items-center h-250px">
-      <div>
-        <div class="sk-folding-cube">
-          <div class="sk-cube1 sk-cube"></div>
-          <div class="sk-cube2 sk-cube"></div>
-          <div class="sk-cube4 sk-cube"></div>
-          <div class="sk-cube3 sk-cube"></div>
-        </div>
-        <div class="bouncing">
-         <span style="--i:1">F</span>
-         <span style="--i:2">E</span>
-         <span style="--i:3">T</span>
-         <span style="--i:4">C</span>
-         <span style="--i:5">H</span>
-         <span style="--i:6">I</span>
-         <span style="--i:7">N</span>
-         <span style="--i:8">G</span>
-         <span style="--i:9"></span>
-         <span style="--i:10">.</span>
-         <span style="--i:11">.</span>
-         <span style="--i:12">.</span>
-        </div>
-      </div>
-    </div>
+    <Loader v-if="ready === false" />
     <div v-if="(type == 1 || type == 2) && ready === true" class="pl-40px pb-100px clearfix">
       <div
         class="item"
@@ -306,10 +171,11 @@
 import { mapState } from 'vuex';
 import { query_user_holding } from '@/config/collection/separate_pool';
 import { query_user_locked } from '@/config/user_info/locked_nft';
+import Loader from '@/components/Loader.vue';
 
 export default {
   props: {},
-  components: {},
+  components: { Loader },
   computed: {
     ...mapState('admin', ['connectStatus']),
     ...mapState(['userInfo']),
